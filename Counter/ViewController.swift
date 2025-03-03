33a9ec28eebaf16a15e216dc51781acb9e868557
m.kozhukhov <neogenka@gmail.com>
2025-03-03 16:13:53 +0300

    Исправлены замечания после ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,58 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    class Counter {
-        var count = 0
-        func currentDate() -> String {
-            let today = Date.now
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "dd-MM-yyyy-hh-mm-ss"
-            var date = dateFormatter.string(from: today)
-            return date
-        }
-        func increment() {
-            count += 1
-        }
-        func decrement() {
-            count -= 1
-        }
-        func reset() {
-            count = 0
-        }
+final class ViewController: UIViewController {
+    var count = 0
+    func currentDate() -> String {
+        let today = Date.now
+        lazy var dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy-hh-mm-ss"
+        var date = dateFormatter.string(from: today)
+        return date
+    }
+    func increment() {
+        count += 1
+    }
+    func decrement() {
+        count -= 1
+    }
+    func reset() {
+        count = 0
     }
     
-    let counter = Counter()
-    
-    
-    
-    @IBOutlet weak var counterText: UILabel!
-    @IBOutlet weak var counterChangeList: UITextView!
+    @IBOutlet private weak var counterText: UILabel!
+    @IBOutlet private weak var counterChangeList: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
-    @IBAction func increaseCount() {
-            counter.increment()
-            counterText.text = String(counter.count)
-            counterChangeList.text += "\n\(counter.currentDate()): значение изменено на +1"
+    @IBAction private func increaseCount() {
+        increment()
+        counterText.text = String(count)
+        counterChangeList.text += "\n\(currentDate()): значение изменено на +1"
     }
     
-    @IBAction func decreaseCount() {
-        if counter.count == 0 {
+    @IBAction private func decreaseCount() {
+        if count == 0 {
             counterChangeList.text += "\nдата: - попытка уменьшить значение меньше 0"
         } else {
-            counter.decrement()
-            counterText.text = String(counter.count)
-            counterChangeList.text += "\n\(counter.currentDate()): значение изменено на -1"
+            decrement()
+            counterText.text = String(count)
+            counterChangeList.text += "\n\(currentDate()): значение изменено на -1"
         }
     }
     
     
-    @IBAction func refreshCount() {
+    @IBAction private func refreshCount() {
         counterText.text = "0"
-        counterChangeList.text += "\n\(counter.currentDate()): значение cброшено"
+        counterChangeList.text += "\n\(currentDate()): значение cброшено"
     }
 }
